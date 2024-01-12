37334f1c3d8b89573f56333ac3fb56541ea8f395
Aishwarya <9a9bhardwaj@gmail.com>
2024-01-12 09:13:55 +0530

    AI chatbot button, UI commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,21 +8,31 @@ export default class Navbar extends Component {
     render() {
         return (
             <NavWrapper className="navbar nav-bar-expand-sm navbar-dark px-sm-5">
-               <Link to='/'>
+               <Link to='/'> 
                   <img src={logo} alt="store" className="navbar-brand" />
                </Link> 
                <ul className="navbar-nav align-items-center">
                  <li className="nav-item ml-5">
+                    
                     <Link to="/" className="nav-link">
                        Products
                     </Link>
                  </li>
                </ul>
+               
                <Link to="/cart" className="ml-auto">
                   <ButtonContainer>
                       <i className="fas fa-cart-plus">my cart</i>
                   </ButtonContainer>
                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+
+                        <a href="http://localhost:5173/" class="btn btn-primary btn-block">AI Chatbot</a>
+                           
+                       
+                    </li>
+                </ul>
             </NavWrapper>
         )
     }
